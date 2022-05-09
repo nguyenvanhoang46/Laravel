9a5151187081f4b0ab7846bc30a52080001a2d56
hoang <nguyenvanhoang4601@gmail.com>
2022-05-09 23:26:33 +0700

    lien ket cac bang

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\DocBlock\Description;
 
 class Category extends Model
 {
     protected $table = 'category';
     protected $primaryKey = 'id';
+    protected $fillable = ['name', 'description'];
+
+    public function Category() {
+    return $this->hasMany(Product::class, 'id_category', 'id');
+}
 
     use HasFactory;
 }
