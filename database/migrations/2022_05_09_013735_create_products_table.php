6c5913c4d76451bba18a8c260edee05dab65cc4d
nguyenvanhoang46 <nguyenvanhoang46@gmail.com>
2022-05-09 09:32:03 +0700

    tao bang

--- database/migrations/2022_05_09_013735_create_products_table.php
+++ database/migrations/2022_05_09_013735_create_products_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->integer("id_category");
+            $table->string("name");
+            $table->integer("price");
+            $table->integer("quantity");
+            $table->string("image");
             $table->timestamps();
         });
     }
@@ -26,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('products');
     }
 };
